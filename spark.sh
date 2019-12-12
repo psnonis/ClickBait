fd802b8daed24fca85cc93b3dd592daf9bfe1ae2
Priyankara Nonis <priyankara.nonis@berkeley.edu>
2019-12-12 00:31:07 +0000

    12-11-19

--- spark.sh
+++ spark.sh
@@ -7,7 +7,7 @@ docker run -d -p 8888:8888 -p 4040:4040 -p 4041:4041 \
 -e GRANT_SUDO=yes \
 -e GEN_CERT=yes \
 -v $PWD:/home/jovyan/work \
--v /slow:/home/jovyan/work/slow \
+-v /slow:/slow \
 jupyter/all-spark-notebook \
 start-notebook.sh \
 --LabApp.password='sha1:39ea6f32055d:7b97c2c76e9b554215a7056e595fda50283df281'
